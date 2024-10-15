0632fe6617fab532605294a46a6c3f3f85910e44
honeok <yihaohey@gmail.com>
2024-10-15 11:00:03 +0800

    Dev and debug ⚡

--- honeok_dev.sh
+++ honeok_dev.sh
@@ -5,6 +5,7 @@
 # Current Author: honeok
 # Blog: https://www.honeok.com
 
+# 细
 yellow='\033[0;33m'       # 黄色
 red='\033[0;31m'          # 红色
 magenta='\033[0;35m'      # 品红色
@@ -14,7 +15,13 @@ cyan='\033[0;36m'         # 青色
 purple='\033[0;35m'       # 紫色
 gray='\033[0;30m'         # 灰色
 orange='\033[0;38;5;208m' # 橙色
+light_yellow='\033[93m'   # 浅黄色
+light_green='\033[92m'    # 亮绿色
+light_cyan='\033[96m'     # 亮青色
+light_purple='\033[95m'    # 亮紫色
 white='\033[0m'           # 白色
+
+# 粗
 #yellow='\033[1;33m'       # 黄色
 #red='\033[1;31m'          # 红色
 #magenta='\033[1;35m'      # 品红色
@@ -25,6 +32,7 @@ white='\033[0m'           # 白色
 #gray='\033[1;30m'         # 灰色
 #orange='\033[1;38;5;208m' # 橙色
 #white='\033[0m'           # 白色
+
 _yellow() { echo -e ${yellow}$@${white}; }
 _red() { echo -e ${red}$@${white}; }
 _magenta() { echo -e ${magenta}$@${white}; }
@@ -34,8 +42,11 @@ _cyan() { echo -e ${cyan}$@${white}; }
 _purple() { echo -e ${purple}$@${white}; }
 _gray() { echo -e ${gray}$@${white}; }
 _orange() { echo -e ${orange}$@${white}; }
+_light_yellow () { echo -e ${light_yellow}$@${white}; }
+_light_green () { echo -e ${light_green}$@${white}; }
+_light_cyan () { echo -e ${light_cyan}$@${white}; }
+_light_purple () { echo -e ${light_purple}$@${white}; }
 
-# 2024/10/14
 honeok_v="v3.0.0_dev"
 ########################################
 # 设置地区相关的代理配置
@@ -81,28 +92,13 @@ set_region_config
 ########################################
 print_logo(){
 	local os_info=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d '"' -f 2)
-	local bold='\033[1m'       # 加粗
-	local logo="
- _                            _    
-| |                          | |   
-| |__   ___  _ __   ___  ___ | | __
-| '_ \ / _ \| '_ \ / _ \/ _ \| |/ /
-| | | | (_) | | | |  __| (_) |   < 
-|_| |_|\___/|_| |_|\___|\___/|_|\_\\"
-
-	# 打印logo
-	echo -e "${cyan}${logo}\\${white}"
-	echo ""
-
-	# 设置工具版本文本
-	#local text="Version: ${honeok_v}"
+echo -e "${light_yellow}   __                      __ 
+  / /  ___  ___  ___ ___  / /__
+ / _ \/ _ \/ _ \/ -_) _ \/  '_/
+/_//_/\___/_//_/\__/\___/_/\_\ 
+"
 	local os_text="当前操作系统:${os_info}"
-	#local padding="                                        "
-
-	# 打印操作系统信息和工具版本信息
-	#echo -e "${yellow}${os_text}${while}"
-	_yellow "${os_text}"
-	#echo -e "${padding}${yellow}${bold}${text}${white}"
+	_light_yellow "${os_text}"
 }
 
 #################### 系统信息START ####################
@@ -8029,28 +8025,32 @@ honeok(){
 
 	while true; do
 		clear
-		echo -e "${yellow}Github: https://github.com/honeok${white}     ${cyan}Version: ${honeok_v}${white}"
-		echo "-------------------------------------------------------"
 		print_logo
-		echo "-------------------------------------------------------"
-		_purple "适配: Ubuntu/Debian/CentOS/Alpine/RedHat/Fedora/Alma/Rocky"
-		_cyan "Author: honeok Blog: www.honeok.com"
-		echo -e "${green}服务器当前时间: $(date +"%Y-%m-%d %H:%M:%S")${white} ${yellow}时区: ${system_time}${white}"
-		echo "-------------------------------------------------------"
-		echo "1. 系统信息查询                   2. 系统更新"
-		echo "3. 系统清理                       4. 常用工具 ▶"
-		echo "5. BBR管理 ▶                      6. Docker管理 ▶"
-		echo "7. WARP管理 ▶                     8. LDNMP建站 ▶"
-		echo "-------------------------------------------------------"
-		echo "12. 面板工具 ▶                    13. 系统工具 ▶"
-		echo "14. 我的工作区 ▶                  15. VPS测试脚本合集 ▶"
-		echo "16. 节点搭建脚本合集 ▶            17. 甲骨文云脚本合集 ▶"
+		_light_purple "适配Ubuntu/Debian/CentOS/Alpine/Kali/Arch/RedHat/Fedora/Alma/Rocky系统"
+		echo -e "${light_cyan}Author: honeok${white} ${light_yellow} ${honeok_v}${white}"
+		echo "------------------------"
+		echo "1. 系统信息查询"
+		echo "2. 系统更新"
+		echo "3. 系统清理"
+		echo "4. 常用工具 ▶"
+		echo "5. BBR管理 ▶"
+		echo "6. Docker管理 ▶"
+		echo "7. WARP管理 ▶"
+		echo "8. LDNMP建站 ▶"
+		echo "12. 面板工具 ▶"
+		echo "13. 系统工具 ▶"
+		echo "14. 我的工作区 ▶"
+		echo "15. VPS测试脚本合集 ▶"
+		echo "16. 节点搭建脚本合集 ▶"
+		echo "17. 甲骨文云脚本合集 ▶"
 		echo "18. 常用环境管理 ▶"
-		echo "-------------------------------------------------------"
+		echo "------------------------"
 		echo "99. 幻兽帕鲁开服脚本 ▶"
-		echo "-------------------------------------------------------"
-		echo "00.脚本更新                       0. 退出"
-		echo "-------------------------------------------------------"
+		echo "------------------------"
+		echo "00.脚本更新"
+		echo "------------------------"
+		echo "0. 退出脚本"
+		echo "------------------------"
 		echo ""
 
 		echo -n -e "${yellow}请输入选项并按回车键确认:${white}"
